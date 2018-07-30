2c871b7be56a1cbda4eb516c05b071495e3a55ef
Kevin RRivera <krivera1015@gmail.com>
2018-07-30 19:39:16 -0400

    Created Find Method

--- app/models/soldier.rb
+++ app/models/soldier.rb
@@ -5,10 +5,16 @@ class Soldier < ActiveRecord::Base
   has_many :battlefields
   has_many :enemies, through: :battlefields
 
+  after_initialize :init
 
-after_initialize :init
+  def arr_abilities
+    ["shoot", "kiss", "kick", "love", "sleep"]
+  end
 
-def init
-  self.hp = 10
-end
+  def init
+    self.hp = 10
+    self.ability = arr_abilities.sample
+  end
+
+
 end
--- bin/run.rb
+++ bin/run.rb
@@ -6,6 +6,4 @@ puts "Welcome Soldier, what is your name?"
 
 name = gets.chomp
 
-soldier_1 = Soldier.create(name: name)
-
-puts "Welcome #{name} your hp is #{soldier_1.hp}"
+soldier_exist(name)
