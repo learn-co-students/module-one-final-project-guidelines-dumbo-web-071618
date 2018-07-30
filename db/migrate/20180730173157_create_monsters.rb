fecd5b6e7c2f2bf4aa915ab80f0dcf8a372b5e83
github username <github email address>
2018-07-30 15:04:37 -0400

    created tables

--- db/migrate/20180730173157_create_monsters.rb
+++ db/migrate/20180730173157_create_monsters.rb
@@ -1,4 +1,9 @@
 class CreateMonsters < ActiveRecord::Migration[5.0]
   def change
+    create_table monsters do |t|
+      t.string :name
+      t.integer :hp
+      t.string :weaknesses
+      
   end
 end
